a81ce01567f551f33376ed3eabc5e8e8972b04fe
Sungsu Kim <narcy85@gmail.com>
2026-02-21 19:28:50 -0500

    make a json file for initial database file for course, Course.db

---
+++ dbinit.js
@@ -0,0 +1,29 @@
+const sqlite3 = require("sqlite3");
+const sqlite = require("sqlite");
+
+async function dbinit(){
+
+
+    const db = await sqlite.open({
+        filename:"Cousre.db",
+        driver:sqlite3.Database
+    });
+
+    await db.exec("DROP TABLE IF EXISTS Course");
+    await db.exec("CREATE TABLE Course (course_code TEXT, course_name TEXT, credit REAL, availability INTEGER, format TEXT, course_type TEXT )");
+
+
+
+    await db.run("INSERT INTO Course VALUES (?,?,?,?,?,?)",['3DM3','Discrete Mathematics', '3.0','1','Online','SFWRTECH Lv3']);
+    await db.run("INSERT INTO Course VALUES (?,?,?,?,?,?)",['3CS3','Computer Security', '3.0','1','Online','SFWRTECH Lv3']);
+    await db.run("INSERT INTO Course VALUES (?,?,?,?,?,?)",['3MA3','Cntmpry Math Sftwre Dvlpmnt', '3.0','1','Online','SFWRTECH Lv3']);
+    await db.run("INSERT INTO Course VALUES (?,?,?,?,?,?)",['3EE3','Engineering Economics', '3.0','1','Online','GENTECH']);
+    await db.run("INSERT INTO Course VALUES (?,?,?,?,?,?)",['4WP3','Advanced Web Programming', '3.0','1','Online','SFWRTECH Lv4']);
+    await db.run("INSERT INTO Course VALUES (?,?,?,?,?,?)",['4EN3','Entrprneurial Thinkng & Innvtn', '3.0','1','Online','GENTECH']);
+    await db.run("INSERT INTO Course VALUES (?,?,?,?,?,?)",['4MC3','Machine Shop', '6.0','1','In-person','MNFTTECH Lv4']);
+    await db.run("INSERT INTO Course VALUES (?,?,?,?,?,?)",['4CC3','Parallel Programming', '3.0','0','Online','SFWRTECH Lv3']);
+
+
+}
+
+dbinit()
