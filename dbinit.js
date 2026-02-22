6f335fb241f92546bd246d4cbf439f3458d6f577
Sungsu Kim <narcy85@gmail.com>
2026-02-21 19:54:47 -0500

    change the data type for availablity as boolean

--- dbinit.js
+++ dbinit.js
@@ -10,7 +10,7 @@ async function dbinit(){
     });
 
     await db.exec("DROP TABLE IF EXISTS Course");
-    await db.exec("CREATE TABLE Course (course_code TEXT, course_name TEXT, credit REAL, availability INTEGER, format TEXT, course_type TEXT )");
+    await db.exec("CREATE TABLE Course (course_code TEXT, course_name TEXT, credit REAL, availability BOOL, format TEXT, course_type TEXT )");
 
 
 
@@ -22,8 +22,8 @@ async function dbinit(){
     await db.run("INSERT INTO Course VALUES (?,?,?,?,?,?)",['4EN3','Entrprneurial Thinkng & Innvtn', '3.0','1','Online','GENTECH']);
     await db.run("INSERT INTO Course VALUES (?,?,?,?,?,?)",['4MC3','Machine Shop', '6.0','1','In-person','MNFTTECH Lv4']);
     await db.run("INSERT INTO Course VALUES (?,?,?,?,?,?)",['4CC3','Parallel Programming', '3.0','0','Online','SFWRTECH Lv3']);
-
-
+     
+    
 }
 
 dbinit()
