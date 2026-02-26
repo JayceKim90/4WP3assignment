44d64ee239753bcc5c7397b94e9118ab86f13de5
Sungsu Kim <narcy85@gmail.com>
2026-02-25 23:18:36 -0500

    add backend input validation when add the course and show error at fromtend

--- app.ctl.js
+++ app.ctl.js
@@ -31,12 +31,25 @@ app.get("/add", async function(req,res){
 
 app.post("/addCourse", async function(req,res) {
     
+        let errors=[];
+        if(isNaN(parseFloat(req.body.credit))|| (parseFloat(req.body.credit))<0.5 || parseFloat(req.body.credit)>6.0)
+        {
+            errors.push("Validation Error: Credit must be a number between 0.5 and 6.0.");
+        }
+        if (!req.body.code|| (typeof req.body.code !== "string")|| req.body.code.length>10)
+        {
+            errors.push("Validation Error: Course Code must be text and under 10 characters.");
+        }
+        if(errors.length>0){
+            const CourseArray = await Model.getAllCourse();
     
+            res.render("main_page",{Courses: CourseArray,showAddForm:true,errors:errors });
+    
+
+        }else{
         await Model.addCourse(req.body.code, req.body.name, req.body.credit, req.body.availability, req.body.format, req.body.type, req.body.description);
-        const CourseArray = await Model.getAllCourse();
-    
-        res.render("main_page",{Courses: CourseArray});
-    
+        res.redirect("/");
+        }
 });
 
 
